3e44455b1a7ccf3b7411e8c6609ccef81780b914
luizfellipes <luiz.13.silva@gmail.com>
2022-08-30 10:54:51 -0400

    Corrigido o retorno de alteração solicitada, onde atualizava todos os cadastros

--- src/main/java/CadastroUsuariosMain.java
+++ src/main/java/CadastroUsuariosMain.java
@@ -9,77 +9,74 @@ import java.util.Scanner;
 
 public class CadastroUsuariosMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
+            String continua = "S";
+            UsuarioService service = new UsuarioService();
+            while (continua == "S") {
+                // Realiza o Cadastro de Usuários.
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("#####################Sistema de Cadastro de Usuários#####################");
+                System.out.println("Informe o nome do Usuário:");
+                String nome = scanner.next();
+                System.out.println("Informe o CPF do Usuário:");
+                String cpf = scanner.next();
+                System.out.println("Informe o email do Usuário:");
+                String email = scanner.next();
+                System.out.println("Informe a data de nascimento do Usuário:");
+                LocalDate dataNascimento = LocalDate.parse(scanner.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                System.out.println("Informe o sexo do Usuário: ");
+                Sexo sexo = scanner.next().equalsIgnoreCase("F") ? Sexo.FEMININO : Sexo.MASCULINO;
+                //Cadastra o novo usuário.
+                Usuario usuario = new Usuario(nome, cpf, email, dataNascimento, sexo);
+                service.save(usuario);
 
 
-        String continua = "S";
-        UsuarioService service = new UsuarioService();
-        while (continua == "S") {
-            // Realiza o Cadastro de Usuários.
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("#####################Sistema de Cadastro de Usuários#####################");
-            System.out.println("Informe o nome do Usuário:");
-            String nome = scanner.next();
-            System.out.println("Informe o CPF do Usuário:");
-            String cpf = scanner.next();
-            System.out.println("Informe o email do Usuário:");
-            String email = scanner.next();
-            System.out.println("Informe a data de nascimento do Usuário:");
-            LocalDate dataNascimento = LocalDate.parse(scanner.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-            System.out.println("Informe o sexo do Usuário: ");
-            Sexo sexo = scanner.next().equalsIgnoreCase("F") ? Sexo.FEMININO : Sexo.MASCULINO;
-            //Cadastra o novo usuário.
-            Usuario usuario = new Usuario(nome, cpf, email, dataNascimento, sexo);
-            service.save(usuario);
-
-
-            //Faz a Atualização das informações desejadas do cadastro.
-            System.out.println("Deseja Atualizar algum usuário ? [S/N]");
-            if (scanner.next().equalsIgnoreCase("S")) {
-                String continuar = "S";
-                while (continuar == "S") {
-                    System.out.println("Qual item da lista acima você deseja Atualizar: ");
-                    String atualizaInfo = scanner.next();
-                    service.updateUsuario(atualizaInfo);
-                    System.out.println("Deseja realizar mais alguma atualização ? [S/N]");
-                    if (scanner.next().equalsIgnoreCase("N")) {
-                        continuar = "N";
+                //Faz a Atualização das informações desejadas do cadastro.
+                System.out.println("Deseja Atualizar algum usuário ? [S/N]");
+                if (scanner.next().equalsIgnoreCase("S")) {
+                    String continuar = "S";
+                    while (continuar == "S") {
+                        System.out.println("Qual item da lista acima você deseja Atualizar: ");
+                        String atualizaInfo = scanner.next();
+                        service.updateUsuario(atualizaInfo);
+                        System.out.println("Deseja realizar mais alguma atualização ? [S/N]");
+                        if (scanner.next().equalsIgnoreCase("N")) {
+                            continuar = "N";
+                        }
                     }
                 }
-            }
 
-            //Realiza a Exclusão de algum cadastro dentro da lista.
-            System.out.println("Deseja excluir algum usuário ? [S/N]");
-            if (scanner.next().equalsIgnoreCase("S")) {
-                System.out.println("Qual ID você deseja deletar: ");
-                int deletarId = scanner.nextInt();
-                boolean deletado = service.delete(deletarId);
-                if (deletado) {
-                    System.out.println("O ID Selecionado: " + deletarId + " foi exluido com sucesso.");
-                } else {
-                    System.out.println("Esse ID: " + deletarId + " não pode ser excluido ou não existe !!!");
+                //Realiza a Exclusão de algum cadastro dentro da lista.
+                System.out.println("Deseja excluir algum usuário ? [S/N]");
+                if (scanner.next().equalsIgnoreCase("S")) {
+                    System.out.println("Qual ID você deseja deletar: ");
+                    int deletarId = scanner.nextInt();
+                    boolean deletado = service.delete(deletarId);
+                    if (deletado) {
+                        System.out.println("O ID Selecionado: " + deletarId + " foi exluido com sucesso.");
+                    } else {
+                        System.out.println("Esse ID: " + deletarId + " não pode ser excluido ou não existe !!!");
+                    }
+                }
+
+                //Busca os cadastros da lista.
+                System.out.println("Deseja ver a lista de usuários cadastrados? [S/N]");
+                if (scanner.next().equalsIgnoreCase("S")) {
+                    List<Usuario> usuariosList = service.findAll();
+                    for (int i = 0; i < usuariosList.size(); i++) {
+                        System.out.println(usuariosList.get(i).toString());
+                    }
+                }
+
+                System.out.println("Deseja continuar? [S/N]");
+                if (scanner.next().equalsIgnoreCase("N")) {
+                    continua = "N";
                 }
             }
-
-            //Busca os cadastros da lista.
-            System.out.println("Deseja ver a lista de usuários cadastrados? [S/N]");
-            if (scanner.next().equalsIgnoreCase("S")) {
-                List<Usuario> usuariosList = service.findAll();
-                for (int i = 0; i < usuariosList.size(); i++) {
-                    System.out.println(usuariosList.get(i).toString());
-                }
-            }
-
-            System.out.println("Deseja continuar? [S/N]");
-            if (scanner.next().equalsIgnoreCase("N")) {
-                continua = "N";
-            }
-
             if (continua.equals("N")) {
-                System.err.println("Fim do programa !");
+               System.err.println("Fim do Programa !!!");
             }
 
-        }
     }
 }
-
--- src/main/java/br/com/desafio/service/UsuarioService.java
+++ src/main/java/br/com/desafio/service/UsuarioService.java
@@ -23,7 +23,6 @@ public class UsuarioService {
         return usuario;
     }
 
-
     public List<Usuario> findAll() {
         return this.usuarios;
     }
@@ -66,58 +65,57 @@ public class UsuarioService {
         return null;
     }
 
-
     //Retorna a alteração solicitada no cadastro.
     public Usuario updateUsuario(String atualizaInfo) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o ID que deseja atualizar: ");
         int id = scanner.nextInt();
-        Usuario updateId = retornaUsuario(id);
-        for (Usuario usuario : usuarios) {
-            if (updateId != null) {
-                switch (atualizaInfo) {
-                    case "nome":
-                        System.out.println("Digite o novo nome: ");
-                        String nome = scanner.next();
-                        usuario.setNome(nome);
-                        System.out.println("Este é o nome escolhido: " + nome);
-                        break;
+        Usuario usuario = retornaUsuario(id);
+        Integer updateId = usuario.getId(id);
+        if (updateId != null) {
+            switch (atualizaInfo) {
+                case "nome":
+                    System.out.println("Digite o novo nome: ");
+                    String nome = scanner.next();
+                    usuario.setNome(nome);
+                    System.out.println("Este é o nome escolhido: " + nome);
+                    break;
 
-                    case "cpf":
-                        System.out.println("Digite o novo cpf: ");
-                        String cpf = scanner.next();
-                        usuario.setCpf(cpf);
-                        System.out.println("Este é o cpf escolhido: " + cpf);
-                        break;
+                case "cpf":
+                    System.out.println("Digite o novo cpf: ");
+                    String cpf = scanner.next();
+                    usuario.setCpf(cpf);
+                    System.out.println("Este é o cpf escolhido: " + cpf);
+                    break;
 
-                    case "email":
-                        System.out.println("Digite o novo email: ");
-                        String email = scanner.next();
-                        usuario.setEmail(email);
-                        System.out.println("Este é o email escolhido: " + email);
-                        break;
+                case "email":
+                    System.out.println("Digite o novo email: ");
+                    String email = scanner.next();
+                    usuario.setEmail(email);
+                    System.out.println("Este é o email escolhido: " + email);
+                    break;
 
-                    case "nascimento":
-                        System.out.println("Digite o nova data do nascimento: ");
-                        LocalDate dataNascimento = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                        usuario.setDataNascimento(dataNascimento);
-                        System.out.println("Esta é a nova data do nascimento escolhido: " + dataNascimento);
-                        break;
+                case "nascimento":
+                    System.out.println("Digite o nova data do nascimento: ");
+                    LocalDate dataNascimento = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                    usuario.setDataNascimento(dataNascimento);
+                    System.out.println("Esta é a nova data do nascimento escolhido: " + dataNascimento);
+                    break;
 
-                    case "sexo":
-                        System.out.println("Digite o novo sexo: ");
-                        Sexo sexo = scanner.next().equals("F") ? Sexo.FEMININO : Sexo.MASCULINO;
-                        usuario.setSexo(sexo);
-                        System.out.println("Este é o sexo escolhido: " + sexo);
-                        break;
+                case "sexo":
+                    System.out.println("Digite o novo sexo: ");
+                    Sexo sexo = scanner.next().equals("F") ? Sexo.FEMININO : Sexo.MASCULINO;
+                    usuario.setSexo(sexo);
+                    System.out.println("Este é o sexo escolhido: " + sexo);
+                    break;
 
-                    default:
-                        System.out.println("Não foi possível realizar a sua alteração, verifique se está correta a informação a ser substituída.");
-                }
-            } else {
-                System.out.print("Id: " + id + " não encontrado.");
+                default:
+                    System.out.println("Não foi possível realizar a sua alteração, verifique se está correta a informação a ser substituída.");
             }
+        } else {
+            System.out.print("Id: " + id + " não encontrado.");
         }
+
         return null;
     }
 
