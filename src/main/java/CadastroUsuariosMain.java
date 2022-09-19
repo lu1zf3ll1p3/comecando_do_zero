d2863d4061c26a784815efa6a6bd2bf74152540c
Luiz Fellipe <luiz.13.silva@gmail.com>
2022-09-19 19:48:28 -0400

    Codigo de atualizar refatorado

--- src/main/java/CadastroUsuariosMain.java
+++ src/main/java/CadastroUsuariosMain.java
@@ -16,20 +16,21 @@ public class CadastroUsuariosMain {
         while (continua == "S") {
             // Realiza o Cadastro de Usu?rios.
             Scanner scanner = new Scanner(System.in);
-            System.out.println("#####################Sistema de Cadastro de Usu?rios#####################");
-            System.out.println("Informe o nome do Usu?rio:");
+            System.out.println("#####################Sistema de Cadastro de Usuários#####################");
+            System.out.println("Informe o nome do Usuário:");
             String nome = scanner.next();
-            System.out.println("Informe o CPF do Usu?rio:");
+            System.out.println("Informe o CPF do Usuário:");
             String cpf = scanner.next();
-            System.out.println("Informe o email do Usu?rio:");
+            System.out.println("Informe o email do Usuário:");
             String email = scanner.next();
-            System.out.println("Informe a data de nascimento do Usu?rio:");
+            System.out.println("Informe a data de nascimento do Usuário:");
             LocalDate dataNascimento = LocalDate.parse(scanner.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-            System.out.println("Informe o sexo do Usu?rio: ");
+            System.out.println("Informe o sexo do Usuário: ");
             Sexo sexo = scanner.next().equalsIgnoreCase("F") ? Sexo.FEMININO : Sexo.MASCULINO;
-            //Cadastra o novo usu?rio.
+            //Cadastra o novo usuário.
             Usuario usuario = new Usuario(nome, cpf, email, dataNascimento, sexo);
             service.save(usuario);
+
 
 
             //Faz a Atualiza??o das informa??es desejadas do cadastro.
@@ -43,17 +44,17 @@ public class CadastroUsuariosMain {
                 System.out.println(usuarioEncontrado.toString());
                 System.out.println("Informe o nome do Usuário:");
                 String novoNome = scanner.next();
-                System.out.println("Informe o CPF do Usu?rio:");
+                System.out.println("Informe o CPF do Usuário:");
                 String novoCpf = scanner.next();
-                System.out.println("Informe o email do Usu?rio:");
+                System.out.println("Informe o email do Usuário:");
                 String novoEmail = scanner.next();
-                System.out.println("Informe a data de nascimento do Usu?rio:");
+                System.out.println("Informe a data de nascimento do Usuário:");
                 LocalDate novaDataNascimento = LocalDate.parse(scanner.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-                System.out.println("Informe o sexo do Usu?rio: ");
+                System.out.println("Informe o sexo do Usuário: ");
                 Sexo novoSexo = scanner.next().equalsIgnoreCase("F") ? Sexo.FEMININO : Sexo.MASCULINO;
                 Usuario novoUsuario = new Usuario(id, novoNome, novoCpf, novoEmail, novaDataNascimento, novoSexo);
                 System.out.println(novoUsuario);
-                System.out.println("Deseja salvar as novas informa??es ?[S/N]");
+                System.out.println("Deseja salvar as novas informarções ?[S/N]");
                 if (scanner.next().equalsIgnoreCase("S")){
                 service.update(id, novoUsuario);
                 System.out.println(novoUsuario);
@@ -66,21 +67,21 @@ public class CadastroUsuariosMain {
 
             }
 
-            //Realiza a Exclus?o de algum cadastro dentro da lista.
-            System.out.println("Deseja excluir algum usu?rio ? [S/N]");
+            //Realiza a Exclusão de algum cadastro dentro da lista.
+            System.out.println("Deseja excluir algum usuário ? [S/N]");
             if (scanner.next().equalsIgnoreCase("S")) {
-                System.out.println("Qual ID voc? deseja deletar: ");
+                System.out.println("Qual ID você deseja deletar: ");
                 int deletarId = scanner.nextInt();
                 boolean deletado = service.delete(deletarId);
                 if (deletado) {
                     System.out.println("O ID Selecionado: " + deletarId + " foi exluido com sucesso.");
                 } else {
-                    System.out.println("Esse ID: " + deletarId + " n?o pode ser excluido ou n?o existe !!!");
+                    System.out.println("Esse ID: " + deletarId + " não pode ser excluido ou não existe !!!");
                 }
             }
 
             //Busca os cadastros da lista.
-            System.out.println("Deseja ver a lista de usu?rios cadastrados? [S/N]");
+            System.out.println("Deseja ver a lista de usuários cadastrados? [S/N]");
             if (scanner.next().equalsIgnoreCase("S")) {
                 List<Usuario> usuariosList = service.findAll();
                 for (int i = 0; i < usuariosList.size(); i++) {
